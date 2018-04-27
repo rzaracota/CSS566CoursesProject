624dd8c1b7561e5002eeaa0dd318fceaa45a0982
RizRam <riz.ramdhani@gmail.com>
2018-04-26 23:27:15 -0700

    -Added Skeleton -Contains Website Project (UI/Front End)

---
+++ Software Management Course Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Software_Management_Course_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

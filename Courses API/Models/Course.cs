5ac3f637f183f7754826c3251ef5554d4b9ee55e
RizRam <riz.ramdhani@gmail.com>
2018-05-03 15:08:40 -0700

    +Added a project for Course API service +Added a project for Module API service

---
+++ Courses API/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public List<int> Modules { get; set; }
+    }
+}
---
+++ Module API/Models/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuleAPI.Models
+{
+    public class Module
+    {
+        public int ModuleID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}

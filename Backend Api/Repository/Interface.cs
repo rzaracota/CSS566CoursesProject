ea03d46c9881922c5ccce58030aaf32e5b9949af
Mitchell Lee <mitchl2@bitbucket.org>
2018-05-12 12:54:01 -0700

    Created interface for the module repository class

---
+++ Backend Api/Repository/Interface.cs
@@ -0,0 +1,14 @@
+﻿using Backend_Api.Repo_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Api.Repository {
+    public interface IModuleRepository {
+        void CreateModule(Module module);
+        void UpdateModule(Module module);
+        void GetModule(string id);
+        void DeleteModule(string id);
+    }
+}

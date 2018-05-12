ba5144f70c12722912b7ae11c89a3a6254f98407
Mitchell Lee <mitchl2@bitbucket.org>
2018-05-12 13:04:58 -0700

    Created DB context

---
+++ Backend Api/Repository/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Api.Repository {
+    /**
+     * Initializes database with EntityFramework.
+     **/
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    }
+}

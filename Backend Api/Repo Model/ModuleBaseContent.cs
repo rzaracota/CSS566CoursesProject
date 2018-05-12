d7e8fa74322f8f3dac2ba7e508e1a21502c78c86
Mitchell Lee <mitchl2@bitbucket.org>
2018-05-12 15:59:47 -0700

    Fixed table relationships

--- Backend Api/Repo Model/ModuleBaseContent.cs
+++ Backend Api/Repo Model/ModuleBaseContent.cs
@@ -8,7 +8,9 @@ namespace Backend_Api.Repo_Model {
      * Contains common fields for module content.
      **/
     public class ModuleBaseContent {
+        public string Id { get; set; }
         public string Heading { get; set; }
         public string Type { get; set; }
+        public string CourseId { get; set; }
     }
 }

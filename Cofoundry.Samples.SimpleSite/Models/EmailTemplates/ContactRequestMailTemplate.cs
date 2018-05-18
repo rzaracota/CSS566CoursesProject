b7fcb979f6756bb33c095e800013f122d6d23ae0
Ioana Preda <for_ioanapreda@hotmail.com>
2018-05-17 21:55:44 -0700

    Checking in Initial Project

---
+++ Cofoundry.Samples.SimpleSite/Models/EmailTemplates/ContactRequestMailTemplate.cs
@@ -0,0 +1,34 @@
+﻿using Cofoundry.Core.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cofoundry.Samples.SimpleSite
+{
+    /// <summary>
+    /// Cofoundry includes a framework for sending mail based around template
+    /// classes and view files rendered using RazorEngine.
+    /// 
+    /// For more information see https://github.com/cofoundry-cms/cofoundry/wiki/Mail
+    /// </summary>
+    public class ContactRequestMailTemplate : IMailTemplate
+    {
+        public string ViewFile
+        {
+            get
+            {
+                return "~/Views/EmailTemplates/ContactRequest";
+            }
+        }
+
+        public string Subject
+        {
+            get
+            {
+                return "New Contact Request";
+            }
+        }
+
+        public ContactRequest Request { get; set; }
+    }
+}

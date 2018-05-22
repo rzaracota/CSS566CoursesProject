b27e170263b315d44ffc57ee1b3d6e80860ec32f
NavUWB <navjodh@uw.edu>
2018-05-22 16:04:59 -0700

    - Added class that maps to JSON data - Added automatic parsing of JSON course module - Added Razor page to display module content - Added Controller to serve page data from JSON file (this can be later expanded to include a JSON string from elsewhere, e.g. a HTTP GET) - Added JSON tags for embedding video and powerpoint slides - Added a custom tag to JSON template so we can render elements in addition to teh ones covered explicitly

---
+++ Software Management Course Website/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Software_Management_Course_Website.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private Models.RootObject items;
+
+        public CoursesController()
+        {
+            // Load JSON here
+            using (StreamReader r = new StreamReader(@"Data/AgileGameDevelopmentModule.json"))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<Models.RootObject>(json);
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(items);
+        }
+    }
+}

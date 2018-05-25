fdb2d24fee4dfbb9cf9f211b04bcea6580f5b589
Mitchell Lee <mitchl2@bitbucket.org>
2018-05-24 21:19:44 -0700

    Implemented titleimage Layout

---
+++ Backend Api/Repo Model/ModuleTitleImageContent.cs
@@ -0,0 +1,23 @@
+﻿using Backend_Api.Repo_Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendApi.Repo_Model
+{
+    public class ModuleTitleImageContent : ModuleBaseContent 
+    {
+        /**
+         * Constructs a new ModuleTitleImageContent.
+         **/
+        public ModuleTitleImageContent()
+        {
+            Type = "titleimage";
+        }
+
+        [JsonProperty(PropertyName = "Link")]
+        public string Link { get; set; }
+    }
+}
